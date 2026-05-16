73261aa184d22dfdc1d9a2d393818977f9bf82ac
Jack Dolan <jack@dolanjack.com>
2026-05-16 14:16:42 -0400

    Migrate ESLint config to flat config for v10 compatibility

---
+++ eslint.config.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const globals = require('globals');
+
+module.exports = [
+  {
+    languageOptions: {
+      ecmaVersion: 2022,
+      globals: {
+        ...globals.browser,
+        ...globals.webextensions,
+      },
+    },
+    rules: {
+      'no-var': 'error',
+      'prefer-const': 'error',
+      'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      'eqeqeq': 'error',
+      'no-console': 'off',
+    },
+  },
+];
